d31cb0a82f79b11efe4d94a59d4c072b3606ef9c
jinyoung <jinyoung.eom@gmail.com>
2015-07-28 19:31:42 +0900

    .

---
+++ web02t/src/main/java/web02t/Hello.java
@@ -0,0 +1,32 @@
+package web02t;
+
+import org.springframework.context.ApplicationContext;
+
+public class Hello {
+
+  @Override
+  public String toString() {
+    return "test..ok";
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hello");
+    // 한글 주석 
+    
+    ApplicationContext context = null;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ web02t/src/test/java/web02t/HelloTest.java
@@ -0,0 +1,5 @@
+package web02t;
+
+public class HelloTest {
+
+}
